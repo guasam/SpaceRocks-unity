20010758fc9ef2ba8b6f08f4e43584180c8ac63c
guasam <guasam@outlook.in>
2020-08-05 09:23:04 +0530

    Singleton used

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Codesbiome.U2D;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,14 +7,44 @@ namespace Spacerocks
 {
     public class GameManager : MonoBehaviour
     {
-        // Start is called before the first frame update
+        public static GameManager Instance = null;
+        public static Vector2 RoomSize;
+        private CameraAspector ca;
+
+        /// <summary>
+        /// Awake is called when script instance is being loaded
+        /// </summary>
+        private void Awake()
+        {
+            // Initialize Singleton
+            singleton();
+
+            // Components cache
+            ca = Camera.main.GetComponent<CameraAspector>();
+
+            // Assign properties
+            RoomSize = ca.roomSize;
+        }
+
+        /// <summary>
+        /// Start is called before the first frame update
+        /// </summary>
         private void Start()
         {
         }
 
-        // Update is called once per frame
+        /// <summary>
+        /// Start is called before the first frame update
+        /// </summary>
         private void Update()
         {
+        }
+
+        private void singleton()
+        {
+            // Initialize singleton
+            if (Instance == null) Instance = this;
+            else if (Instance != this) Destroy(gameObject);
         }
     }
 }
